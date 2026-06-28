5f10fe4bfcb0b842c278579f57c9e7421e1cfbf2
moviemembership <moviemembership2023@gmail.com>
2026-06-29 04:35:21 +0800

    Update mail configuration for SMTP settings

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import smtplib
 app = Flask(__name__)
 
 app.config["MAIL_SERVER"] = "smtp.hostinger.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_TIMEOUT"] = 10
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
